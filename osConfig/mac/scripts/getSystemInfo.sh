aece0d8af55788d8f87faa8d386f224da8bdf9a2
Evan Harmon <evan@evanharmon.com>
2023-07-14 18:23:44 -0500

    updateMac.sh refinements
    Resolve conflict
    
    update
    
    refinements
    
    update
    
    Add pre-commit
    
    pre-commit ci
    
    Setup updateMac.sh cronjob
    
    Add precommit to Github Actions
    
    [pre-commit.ci lite] apply automatic fixes
    
    aws bug
    
    github action edit
    
    fix action
    
    More validation

--- osConfig/mac/scripts/getSystemInfo.sh
+++ osConfig/mac/scripts/getSystemInfo.sh
@@ -1,9 +1,11 @@
-#!/bin/zsh
+#!/bin/sh
+ifconfig -a
 sysctl kern.version
 sysctl kern.ostype
 sysctl kern.osrelease
 sysctl kern.osrevision
 uname -a
+hostname -f
 system_profiler SPSoftwareDataType SPHardwareDataType
 sw_vers
 neofetch
