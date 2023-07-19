9f4c3bbd6b8f43b1f6d0ad1a0de0f3519a456462
Evan Harmon <evan@evanharmon.com>
2023-07-19 09:55:31 -0500

    Update (#13)
    * updateMac.sh refinements
    
    Resolve conflict
    
    update
    
    refinements
    
    update
    
    Add pre-commit
    
    pre-commit ci
    
    Setup updateMac.sh cronjob
    
    Add precommit to Github Actions
    
    [pre-commit.ci lite] apply automatic fixes
    
    aws bug
    
    github action edit
    
    fix action
    
    More validation
    
    * Fix pre-commit
    
    * push
    
    * update
    
    * updating

--- osConfig/languages/javaScript/setupJavaScript.sh
+++ osConfig/languages/javaScript/setupJavaScript.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # Script to setup nodejs on a new developer environment.
 # Installing npm packages globally with a package.json file is not supported.
@@ -6,7 +6,20 @@
 echo -e "\033[1;32m  ......Starting setupNode.sh......  \033[0m"
 
 # Base packages
-npm install -g tldr #https://github.com/tldr-pages/tldr
+npm install grunt --location=global #https://github.com/tldr-pages/tldr
+npm install gulp --location=global
+npm install pm2 --location=global
+npm install moment --location=global
+npm install babel-cli --location=global
+npm install socketio --location=global
+npm install dotenv --location=global
+npm install puppeteer --location=global
+npm install --location=global webpack
+npm install --location=global jest
+npm install --location=global mocha
+npm install --location=global chai
+npm install --location=global graphql
+
 
 # Optional packages
 #npm install -g gitlab-backup-util-harduino #https://www.npmjs.com/package/gitlab-backup-util-harduino
